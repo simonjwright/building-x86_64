bdda7f6b95a25afcb8dd6ea585c2263332126096
Simon Wright <simon@pushface.org>
2018-11-06 11:46:50 +0000

    GCC 9.0.0 development
      * common.sh (VERSION): 9.0.0.
        (GCC_SRC): use git mirror.
        (GCC_BLD): new.
        (GCC_BOOT_LDFLAGS): renamed from GCC_BOOT_FLAGS.
      * gnat_util.sh: make with GCC_{BLD,SRC}_BASE from GCC_{BLD,SRC}.
      * gnatcoll_bindings.sh: clean each target before build.
          Use (new?) --gpr-opts switch.
      * gnatcoll_db.sh: clean each target before build.

--- common.sh
+++ common.sh
@@ -1,15 +1,18 @@
 # Ensure the GCC source tree already has required versions of GMP,
 # MPFR, MPC installed via contrib/download_prerequisites.
 
-VERSION=8.1.0
+VERSION=9.0.0
 TOP=/Volumes/Miscellaneous
 PREFIX=$TOP/tmp/opt/gcc-$VERSION
 BUILD=x86_64-apple-darwin15
 
 SRC_PATH=$TOP/tmp
-GCC_SRC=$SRC_PATH/gcc-$VERSION
+##### These are for development only
+GCC_SRC=$SRC_PATH/gcc
+GCC_BLD=$SRC_PATH/gcc-265766-build
+#####
 
-GCC_BOOT_FLAGS="-static-libstdc++ -static-libgcc -Wl,-headerpad_max_install_names"
+GCC_BOOT_LDFLAGS="-static-libstdc++ -static-libgcc -Wl,-headerpad_max_install_names"
 
 NEW_PATH=$PREFIX/bin:$PATH
 
